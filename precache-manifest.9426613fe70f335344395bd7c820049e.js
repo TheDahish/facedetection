1f6b9df392a950b980c838165641118ec3b5d9ba
Aldahish <khaled-dahish@hotmail.com>
2020-04-23 19:23:59 +0200

    Updates

---
+++ precache-manifest.9426613fe70f335344395bd7c820049e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62935f03814d1f6c8987a39e7e8bcca6",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "2b77fa0f9b210e2e3291",
+    "url": "/facedetection/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "bcd119c870441c8f142f",
+    "url": "/facedetection/static/css/main.16384475.chunk.css"
+  },
+  {
+    "revision": "2b77fa0f9b210e2e3291",
+    "url": "/facedetection/static/js/2.ee742774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facedetection/static/js/2.ee742774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd119c870441c8f142f",
+    "url": "/facedetection/static/js/main.3b956fdb.chunk.js"
+  },
+  {
+    "revision": "d200b75229000bb47b53",
+    "url": "/facedetection/static/js/runtime-main.19f68930.js"
+  }
+]);
